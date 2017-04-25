a24c242c98736a10541eaca927ee07ffa5de059e
Mario Amrehn <mario.amrehn@fau.de>
2017-04-25 16:44:42 +0200

    [Feature] add support for direct ndarray input images.
    Also: PEP8 and some minor fixes and (minimal) performance improvements.

--- pyimgsaliency/saliency.py
+++ pyimgsaliency/saliency.py
@@ -1,8 +1,8 @@
 import math
-import sys
-import operator
+# import sys
+# import operator
 import networkx as nx
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import scipy.spatial.distance
 import scipy.signal
@@ -10,242 +10,238 @@ import skimage
 import skimage.io
 from skimage.segmentation import slic
 from skimage.util import img_as_float
-from scipy.optimize import minimize
+# from scipy.optimize import minimize
+# import pdb
 
-import pdb
 
-def S(x1,x2,geodesic,sigma_clr=10):
-	return math.exp(-pow(geodesic[x1,x2],2)/(2*sigma_clr*sigma_clr))
+def _func_s(x1, x2, geodesic, sigma_clr=10):  # called by rbd method
+    return math.exp(-pow(geodesic[x1, x2], 2) / (2 * sigma_clr * sigma_clr))
 
-def compute_saliency_cost(smoothness,w_bg,wCtr):
-	n = len(w_bg)
-	A = np.zeros((n,n))
-	b = np.zeros((n))
 
-	for x in xrange(0,n):
-		A[x,x] = 2 * w_bg[x] + 2 * (wCtr[x])
-		b[x] = 2 * wCtr[x]
-		for y in xrange(0,n):
-			A[x,x] += 2 * smoothness[x,y]
-			A[x,y] -= 2 * smoothness[x,y]
-	
-	x = np.linalg.solve(A, b)
+def _compute_saliency_cost(smoothness, w_bg, w_ctr):  # called by rbd method
+    n = len(w_bg)
+    arr = np.zeros((n, n))
+    b = np.zeros(n)
 
-	return x
+    for x in range(0, n):
+        arr[x, x] = 2 * w_bg[x] + 2 * w_ctr[x]
+        b[x] = 2 * w_ctr[x]
+        for y in range(0, n):
+            arr[x, x] += 2 * smoothness[x, y]
+            arr[x, y] -= 2 * smoothness[x, y]
 
-def path_length(path,G):
-	dist = 0.0
-	for i in xrange(1,len(path)):
-		dist += G[path[i - 1]][path[i]]['weight']
-	return dist
+    x = np.linalg.solve(arr, b)
+    return x
 
-def make_graph(grid):
-	# get unique labels
-	vertices = np.unique(grid)
- 
-	# map unique labels to [1,...,num_labels]
-	reverse_dict = dict(zip(vertices,np.arange(len(vertices))))
-	grid = np.array([reverse_dict[x] for x in grid.flat]).reshape(grid.shape)
-   
-	# create edges
-	down = np.c_[grid[:-1, :].ravel(), grid[1:, :].ravel()]
-	right = np.c_[grid[:, :-1].ravel(), grid[:, 1:].ravel()]
-	all_edges = np.vstack([right, down])
-	all_edges = all_edges[all_edges[:, 0] != all_edges[:, 1], :]
-	all_edges = np.sort(all_edges,axis=1)
-	num_vertices = len(vertices)
-	edge_hash = all_edges[:,0] + num_vertices * all_edges[:, 1]
-	# find unique connections
-	edges = np.unique(edge_hash)
-	# undo hashing
-	edges = [[vertices[x%num_vertices],
-			  vertices[x/num_vertices]] for x in edges] 
- 
-	return vertices, edges
-	
 
-def get_saliency_rbd(img_path):
+def _path_length(path, graph):  # called by rbd method
+    return np.sum([graph[path[i - 1]][path[i]]['weight'] for i in range(1, len(path))])
 
-	# Saliency map calculation based on:
-	# Saliency Optimization from Robust Background Detection, Wangjiang Zhu, Shuang Liang, Yichen Wei and Jian Sun, IEEE Conference on Computer Vision and Pattern Recognition (CVPR), 2014
 
-	img = skimage.io.imread(img_path)
+def _make_graph(grid):  # called by rbd method
+    # get unique labels
+    vertices = np.unique(grid)
 
-	if len(img.shape) != 3: # got a grayscale image
-		img = skimage.color.gray2rgb(img)
+    # map unique labels to [1,...,num_labels]
+    reverse_dict = dict(zip(vertices, np.arange(len(vertices))))
+    grid = np.array([reverse_dict[x] for x in grid.flat]).reshape(grid.shape)
 
-	img_lab = img_as_float(skimage.color.rgb2lab(img))
+    # create edges
+    down = np.c_[grid[:-1, :].ravel(), grid[1:, :].ravel()]
+    right = np.c_[grid[:, :-1].ravel(), grid[:, 1:].ravel()]
+    all_edges = np.vstack([right, down])
+    all_edges = all_edges[all_edges[:, 0] != all_edges[:, 1], :]
+    all_edges = np.sort(all_edges, axis=1)
+    num_vertices = len(vertices)
+    edge_hash = all_edges[:, 0] + num_vertices * all_edges[:, 1]
+    # find unique connections
+    edges = np.unique(edge_hash)
+    # undo hashing
+    edges = [[vertices[int(x % num_vertices)],
+              vertices[int(x / num_vertices)]] for x in edges]
 
-	img_rgb = img_as_float(img)
+    return vertices, edges
 
-	img_gray = img_as_float(skimage.color.rgb2gray(img))
 
-	segments_slic = slic(img_rgb, n_segments=250, compactness=10, sigma=1, enforce_connectivity=False)
+def get_saliency_rbd(img):
+    # Saliency map calculation based on:
+    # Saliency Optimization from Robust Background Detection, Wangjiang Zhu, Shuang Liang, Yichen Wei and Jian Sun,
+    # IEEE Conference on Computer Vision and Pattern Recognition (CVPR), 2014
 
-	num_segments = len(np.unique(segments_slic))
+    if isinstance(img, str):  # img is img_path string
+        img = skimage.io.imread(img)
 
-	nrows, ncols = segments_slic.shape
-	max_dist = math.sqrt(nrows*nrows + ncols*ncols)
+    if len(img.shape) != 3:  # got a grayscale image
+        img = skimage.color.gray2rgb(img)
 
-	grid = segments_slic
+    img_lab = img_as_float(skimage.color.rgb2lab(img))
 
-	(vertices,edges) = make_graph(grid)
+    img_rgb = img_as_float(img)
 
-	gridx, gridy = np.mgrid[:grid.shape[0], :grid.shape[1]]
+    img_gray = img_as_float(skimage.color.rgb2gray(img))
 
-	centers = dict()
-	colors = dict()
-	distances = dict()
-	boundary = dict()
+    segments_slic = slic(img_rgb, n_segments=250, compactness=10, sigma=1, enforce_connectivity=False)
 
-	for v in vertices:
-		centers[v] = [gridy[grid == v].mean(), gridx[grid == v].mean()]
-		colors[v] = np.mean(img_lab[grid==v],axis=0)
+    # num_segments = len(np.unique(segments_slic))
 
-		x_pix = gridx[grid == v]
-		y_pix = gridy[grid == v]
+    nrows, ncols = segments_slic.shape
+    max_dist = math.sqrt(nrows * nrows + ncols * ncols)
 
-		if np.any(x_pix == 0) or np.any(y_pix == 0) or np.any(x_pix == nrows - 1) or np.any(y_pix == ncols - 1):
-			boundary[v] = 1
-		else:
-			boundary[v] = 0
+    grid = segments_slic
 
-	G = nx.Graph()
+    vertices, edges = _make_graph(grid)
 
-	#buid the graph
-	for edge in edges:
-		pt1 = edge[0]
-		pt2 = edge[1]
-		color_distance = scipy.spatial.distance.euclidean(colors[pt1],colors[pt2])
-		G.add_edge(pt1, pt2, weight=color_distance )
+    gridx, gridy = np.mgrid[:grid.shape[0], :grid.shape[1]]
 
-	#add a new edge in graph if edges are both on boundary
-	for v1 in vertices:
-		if boundary[v1] == 1:
-			for v2 in vertices:
-				if boundary[v2] == 1:
-					color_distance = scipy.spatial.distance.euclidean(colors[v1],colors[v2])
-					G.add_edge(v1,v2,weight=color_distance)
+    centers = dict()
+    colors = dict()
+    # distances = dict()
+    boundary = dict()
 
-	geodesic = np.zeros((len(vertices),len(vertices)),dtype=float)
-	spatial = np.zeros((len(vertices),len(vertices)),dtype=float)
-	smoothness = np.zeros((len(vertices),len(vertices)),dtype=float)
-	adjacency = np.zeros((len(vertices),len(vertices)),dtype=float)
+    for v in vertices:
+        centers[v] = [gridy[grid == v].mean(), gridx[grid == v].mean()]
+        colors[v] = np.mean(img_lab[grid == v], axis=0)
 
-	sigma_clr = 10.0
-	sigma_bndcon = 1.0
-	sigma_spa = 0.25
-	mu = 0.1
+        x_pix = gridx[grid == v]
+        y_pix = gridy[grid == v]
 
-	all_shortest_paths_color = nx.shortest_path(G,source=None,target=None,weight='weight')
+        if np.any(x_pix == 0) or np.any(y_pix == 0) or np.any(x_pix == nrows - 1) or np.any(y_pix == ncols - 1):
+            boundary[v] = 1
+        else:
+            boundary[v] = 0
 
-	for v1 in vertices:
-		for v2 in vertices:
-			if v1 == v2:
-				geodesic[v1,v2] = 0
-				spatial[v1,v2] = 0
-				smoothness[v1,v2] = 0
-			else:
-				geodesic[v1,v2] = path_length(all_shortest_paths_color[v1][v2],G)
-				spatial[v1,v2] = scipy.spatial.distance.euclidean(centers[v1],centers[v2]) / max_dist
-				smoothness[v1,v2] = math.exp( - (geodesic[v1,v2] * geodesic[v1,v2])/(2.0*sigma_clr*sigma_clr)) + mu 
+    graph = nx.Graph()
 
-	for edge in edges:
-		pt1 = edge[0]
-		pt2 = edge[1]
-		adjacency[pt1,pt2] = 1
-		adjacency[pt2,pt1] = 1
+    # build the graph
+    for edge in edges:
+        pt1 = edge[0]
+        pt2 = edge[1]
+        color_distance = scipy.spatial.distance.euclidean(colors[pt1], colors[pt2])
+        graph.add_edge(pt1, pt2, weight=color_distance)
 
-	for v1 in vertices:
-		for v2 in vertices:
-			smoothness[v1,v2] = adjacency[v1,v2] * smoothness[v1,v2]
+    # add a new edge in graph if edges are both on boundary
+    for v1 in vertices:
+        if boundary[v1] == 1:
+            for v2 in vertices:
+                if boundary[v2] == 1:
+                    color_distance = scipy.spatial.distance.euclidean(colors[v1], colors[v2])
+                    graph.add_edge(v1, v2, weight=color_distance)
 
-	area = dict()
-	len_bnd = dict()
-	bnd_con = dict()
-	w_bg = dict()
-	ctr = dict()
-	wCtr = dict()
+    geodesic = np.zeros((len(vertices), len(vertices)), dtype=float)
+    spatial = np.zeros((len(vertices), len(vertices)), dtype=float)
+    smoothness = np.zeros((len(vertices), len(vertices)), dtype=float)
+    adjacency = np.zeros((len(vertices), len(vertices)), dtype=float)
 
-	for v1 in vertices:
-		area[v1] = 0
-		len_bnd[v1] = 0
-		ctr[v1] = 0
-		for v2 in vertices:
-			d_app = geodesic[v1,v2]
-			d_spa = spatial[v1,v2]
-			w_spa = math.exp(- ((d_spa)*(d_spa))/(2.0*sigma_spa*sigma_spa))
-			area_i = S(v1,v2,geodesic)
-			area[v1] += area_i
-			len_bnd[v1] += area_i * boundary[v2]
-			ctr[v1] += d_app * w_spa
-		bnd_con[v1] = len_bnd[v1] / math.sqrt(area[v1])
-		w_bg[v1] = 1.0 - math.exp(- (bnd_con[v1]*bnd_con[v1])/(2*sigma_bndcon*sigma_bndcon))
+    sigma_clr = 10.0
+    sigma_bndcon = 1.0
+    sigma_spa = 0.25
+    mu = 0.1
 
-	for v1 in vertices:
-		wCtr[v1] = 0
-		for v2 in vertices:
-			d_app = geodesic[v1,v2]
-			d_spa = spatial[v1,v2]
-			w_spa = math.exp(- (d_spa*d_spa)/(2.0*sigma_spa*sigma_spa))
-			wCtr[v1] += d_app * w_spa *  w_bg[v2]
+    all_shortest_paths_color = nx.shortest_path(graph, source=None, target=None, weight='weight')
 
-	# normalise value for wCtr
+    for v1 in vertices:
+        for v2 in vertices:
+            if v1 == v2:
+                geodesic[v1, v2] = 0
+                spatial[v1, v2] = 0
+                smoothness[v1, v2] = 0
+            else:
+                geodesic[v1, v2] = _path_length(all_shortest_paths_color[v1][v2], graph)
+                spatial[v1, v2] = scipy.spatial.distance.euclidean(centers[v1], centers[v2]) / max_dist
+                smoothness[v1, v2] = math.exp(- (geodesic[v1, v2] * geodesic[v1, v2]) / (2.0 * sigma_clr * sigma_clr)) + mu
 
-	min_value = min(wCtr.values())
-	max_value = max(wCtr.values())
+    for edge in edges:
+        pt1 = edge[0]
+        pt2 = edge[1]
+        adjacency[pt1, pt2] = 1
+        adjacency[pt2, pt1] = 1
 
-	minVal = [key for key, value in wCtr.iteritems() if value == min_value]
-	maxVal = [key for key, value in wCtr.iteritems() if value == max_value]
+    for v1 in vertices:
+        for v2 in vertices:
+            smoothness[v1, v2] = adjacency[v1, v2] * smoothness[v1, v2]
 
-	for v in vertices:
-		wCtr[v] = (wCtr[v] - min_value)/(max_value - min_value)
+    area = dict()
+    len_bnd = dict()
+    bnd_con = dict()
+    w_bg = dict()
+    ctr = dict()
+    w_ctr = dict()
 
-	img_disp1 = img_gray.copy()
-	img_disp2 = img_gray.copy()
+    for v1 in vertices:
+        area[v1] = 0
+        len_bnd[v1] = 0
+        ctr[v1] = 0
+        for v2 in vertices:
+            d_app = geodesic[v1, v2]
+            d_spa = spatial[v1, v2]
+            w_spa = math.exp(- (d_spa * d_spa) / (2.0 * sigma_spa * sigma_spa))
+            area_i = _func_s(v1, v2, geodesic)
+            area[v1] += area_i
+            len_bnd[v1] += area_i * boundary[v2]
+            ctr[v1] += d_app * w_spa
+        bnd_con[v1] = len_bnd[v1] / math.sqrt(area[v1])
+        w_bg[v1] = 1.0 - math.exp(- (bnd_con[v1] * bnd_con[v1]) / (2 * sigma_bndcon * sigma_bndcon))
 
-	x = compute_saliency_cost(smoothness,w_bg,wCtr)
+    for v1 in vertices:
+        w_ctr[v1] = 0
+        for v2 in vertices:
+            d_app = geodesic[v1, v2]
+            d_spa = spatial[v1, v2]
+            w_spa = math.exp(- (d_spa * d_spa) / (2.0 * sigma_spa * sigma_spa))
+            w_ctr[v1] += d_app * w_spa * w_bg[v2]
 
-	for v in vertices:
-		img_disp1[grid == v] = x[v]
+    # normalise value for w_ctr
 
-	img_disp2 = img_disp1.copy()
-	sal = np.zeros((img_disp1.shape[0],img_disp1.shape[1],3))
+    min_value = min(w_ctr.values())
+    max_value = max(w_ctr.values())
 
-	sal = img_disp2
-	sal_max = np.max(sal)
-	sal_min = np.min(sal)
-	sal = 255 * ((sal - sal_min) / (sal_max - sal_min))
+    # minVal = [key for key, value in w_ctr.iteritems() if value == min_value]
+    # maxVal = [key for key, value in w_ctr.iteritems() if value == max_value]
 
-	return sal
+    for v in vertices:
+        w_ctr[v] = (w_ctr[v] - min_value) / (max_value - min_value)
 
-def get_saliency_ft(img_path):
+    sal = img_gray.copy()
 
-	# Saliency map calculation based on:
+    x = _compute_saliency_cost(smoothness, w_bg, w_ctr)
 
-	img = skimage.io.imread(img_path)
+    for v in vertices:
+        sal[grid == v] = x[v]
 
-	img_rgb = img_as_float(img)
+    sal_max = np.max(sal)
+    sal_min = np.min(sal)
+    sal = 255 * ((sal - sal_min) / (sal_max - sal_min))  # normalize to [0, 255[
 
-	img_lab = skimage.color.rgb2lab(img_rgb) 
+    return sal
 
-	mean_val = np.mean(img_rgb,axis=(0,1))
 
-	kernel_h = (1.0/16.0) * np.array([[1,4,6,4,1]])
-	kernel_w = kernel_h.transpose()
+def get_saliency_ft(img):
+    # Saliency map calculation based on:
 
-	blurred_l = scipy.signal.convolve2d(img_lab[:,:,0],kernel_h,mode='same')
-	blurred_a = scipy.signal.convolve2d(img_lab[:,:,1],kernel_h,mode='same')
-	blurred_b = scipy.signal.convolve2d(img_lab[:,:,2],kernel_h,mode='same')
+    if isinstance(img, str):  # img is img_path string
+        img = skimage.io.imread(img)
 
-	blurred_l = scipy.signal.convolve2d(blurred_l,kernel_w,mode='same')
-	blurred_a = scipy.signal.convolve2d(blurred_a,kernel_w,mode='same')
-	blurred_b = scipy.signal.convolve2d(blurred_b,kernel_w,mode='same')
+    img_rgb = img_as_float(img)
 
-	im_blurred = np.dstack([blurred_l,blurred_a,blurred_b])
+    img_lab = skimage.color.rgb2lab(img_rgb)
 
-	sal = np.linalg.norm(mean_val - im_blurred,axis = 2)
-	sal_max = np.max(sal)
-	sal_min = np.min(sal)
-	sal = 255 * ((sal - sal_min) / (sal_max - sal_min))
-	return sal
+    mean_val = np.mean(img_rgb, axis=(0, 1))
+
+    kernel_h = (1.0 / 16.0) * np.array([[1, 4, 6, 4, 1]])
+    kernel_w = kernel_h.transpose()
+
+    blurred_l = scipy.signal.convolve2d(img_lab[:, :, 0], kernel_h, mode='same')
+    blurred_a = scipy.signal.convolve2d(img_lab[:, :, 1], kernel_h, mode='same')
+    blurred_b = scipy.signal.convolve2d(img_lab[:, :, 2], kernel_h, mode='same')
+
+    blurred_l = scipy.signal.convolve2d(blurred_l, kernel_w, mode='same')
+    blurred_a = scipy.signal.convolve2d(blurred_a, kernel_w, mode='same')
+    blurred_b = scipy.signal.convolve2d(blurred_b, kernel_w, mode='same')
+
+    im_blurred = np.dstack([blurred_l, blurred_a, blurred_b])
+
+    sal = np.linalg.norm(mean_val - im_blurred, axis=2)
+    sal_max = np.max(sal)
+    sal_min = np.min(sal)
+    sal = 255 * ((sal - sal_min) / (sal_max - sal_min))  # normalize to [0, 255[
+    return sal
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='pyimgsaliency',
-      version='0.1',
+      version='0.1.1',
       description='A package for calculating image saliency',
-      url='https://github.com/yhenon/pyimgsaliency',
+      url='https://github.com/mamrehn/pyimgsaliency',
       author='Yann Henon',
       author_email='none',
       license='Apache',
